4c6369c678f8efbcf046fca3cadfd5d112a0d17c
Norbert Bonnici <norbert@bonnici.co>
2020-01-23 18:31:11 +0100

    Add access to generic attributes
    Crash on accessing attributes of a generic in packages.
    Work around is to access attribute of a constant created from the generic.

--- vhdl_2008/tb_generics_in_packages.vhd
+++ vhdl_2008/tb_generics_in_packages.vhd
@@ -2,50 +2,54 @@ library ieee;
   use ieee.std_logic_1164.all;
 
 package generic_pkg_2 is
-  generic (
-    value : integer;
-    bit_value : std_logic;
-    vector_value : std_logic_vector);
+    generic (
+        value : integer;
+        bit_value : std_logic;
+        vector_value : std_ulogic_vector);
+  
+    constant my_value : integer := value;
+    constant my_bit_value : std_logic := bit_value;
+    constant my_vector_value : std_ulogic_vector := vector_value;
+    constant my_vector_length_fail : integer := vector_value'LENGTH;
+    constant my_vector_length_pass : integer := my_vector_value'LENGTH;
 
-  constant my_value : integer := value;
-  constant my_bit_value : std_logic := bit_value;
-  constant my_vector_value : std_logic_vector := vector_value;
-
-end package;
-
-library ieee;
+  end package;
+  
+  library ieee;
   use ieee.std_logic_1164.all;
 
-package my_pkg_2 is new work.generic_pkg_2
-  generic map (
-    value => 17,
-    bit_value => '1',
-    vector_value => "0000");
+  package my_pkg_2 is new work.generic_pkg_2
+    generic map (
+        value => 17,
+        bit_value => '1',
+        vector_value => "0000");
 
-library ieee;
-  use ieee.std_logic_1164.all;
+  library ieee;
+  use ieee.std_logic_1164.all;      
 
-library vunit_lib;
-context vunit_lib.vunit_context;
-
-entity tb_generics_in_packages is
-  generic (
-    runner_cfg : runner_cfg_t);
-end entity;
-
-architecture tb of tb_generics_in_packages is
-begin
-  test_runner: process is
-    variable my_bit_value : std_logic := '1';
-    variable my_vector_value : std_ulogic_vector := "0000";
+  library vunit_lib;
+  context vunit_lib.vunit_context;
+  
+  entity tb_generics_in_packages is
+    generic (
+      runner_cfg : runner_cfg_t);
+  end entity;
+  
+  architecture tb of tb_generics_in_packages is
   begin
-    test_runner_setup(runner, runner_cfg);
-
-    check_equal(work.my_pkg_2.my_value, 17);
-    check_equal(work.my_pkg_2.my_bit_value, my_bit_value);
-    check_equal(work.my_pkg_2.my_vector_value, my_vector_value);
-
-    test_runner_cleanup(runner);
-    wait;
-  end process test_runner;
-end architecture tb;
+    test_runner: process is
+      variable my_bit_value : std_logic := '1';
+      variable my_vector_value : std_ulogic_vector(3 downto 0) := "0000";
+    begin
+      test_runner_setup(runner, runner_cfg);
+  
+      check_equal(work.my_pkg_2.my_value, 17);
+      check_equal(work.my_pkg_2.my_bit_value, my_bit_value);
+      check_equal(work.my_pkg_2.my_vector_value, my_vector_value);
+      check_equal(work.my_pkg_2.my_vector_length_fail, 4);
+      check_equal(work.my_pkg_2.my_vector_length_pass, 4);
+  
+      test_runner_cleanup(runner);
+      wait;
+    end process test_runner;
+  end architecture tb;
