98ec125ad67c826d996eb046f280c1c7beb19287
Norbert Bonnici <norbert@bonnici.co>
2020-01-23 17:44:46 +0100

    check_equal fix
    Variables required

--- vhdl_2008/tb_generics_in_packages.vhd
+++ vhdl_2008/tb_generics_in_packages.vhd
@@ -22,6 +22,9 @@ package my_pkg_2 is new work.generic_pkg_2
     bit_value => '1',
     vector_value => "0000");
 
+library ieee;
+  use ieee.std_logic_1164.all;
+
 library vunit_lib;
 context vunit_lib.vunit_context;
 
@@ -33,12 +36,14 @@ end entity;
 architecture tb of tb_generics_in_packages is
 begin
   test_runner: process is
+    variable my_bit_value : std_logic := '1';
+    variable my_vector_value : std_ulogic_vector := "0000";
   begin
     test_runner_setup(runner, runner_cfg);
 
     check_equal(work.my_pkg_2.my_value, 17);
-    check_equal(work.my_pkg_2.my_bit_value, '1');
-    check_equal(work.my_pkg_2.my_vector_value, "0000");
+    check_equal(work.my_pkg_2.my_bit_value, my_bit_value);
+    check_equal(work.my_pkg_2.my_vector_value, my_vector_value);
 
     test_runner_cleanup(runner);
     wait;
