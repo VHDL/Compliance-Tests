9278be966e96574dd03d2327e538b048a89e1eee
umarcor <unai.martinezcorral@ehu.eus>
2021-09-21 18:24:01 +0200

    style

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from os.path import join, dirname
+from pathlib import Path
 from vunit import VUnit, VUnitCLI
 from vunit.sim_if.factory import SIMULATOR_FACTORY
-from run_support import run_with_compile_errors
+from run_support import vunit_from_args, run_with_compile_errors
 
-root = dirname(__file__)
+ROOT = Path(__file__).resolve().parent
 
 vhdl_standard = (
     "2019" if SIMULATOR_FACTORY.select_simulator().name == "rivierapro" else "2008"
@@ -18,14 +18,10 @@ args = VUnitCLI().parse_args()
 if "keep_going" in args:
     args.keep_going = True
 
-ui = VUnit.from_args(args, vhdl_standard=vhdl_standard)
-vhdl_2008 = ui.add_library("vhdl_2008")
-vhdl_2019 = ui.add_library("vhdl_2019")
-vhdl_2008.add_source_files(join(root, "vhdl_2008", "*.vhd"))
-vhdl_2019.add_source_files(join(root, "vhdl_2019", "*.vhd"))
+UI = vunit_from_args(args, vhdl_standard)
 
 if "keep_going" in args:
-    ui.main()
+    UI.main()
 else:
     # Workaround while keep_going isn't supported
-    run_with_compile_errors(ui, args, vhdl_standard)
+    run_with_compile_errors(UI, args, vhdl_standard)
