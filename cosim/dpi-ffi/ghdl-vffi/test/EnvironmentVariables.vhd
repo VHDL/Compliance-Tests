831dd8bdc0cd47a73e280bb301b3c13e87e4c483
umarcor <unai.martinezcorral@ehu.eus>
2022-02-02 22:02:40 +0100

    WIP setenv

--- cosim/dpi-ffi/ghdl-vffi/test/EnvironmentVariables.vhd
+++ cosim/dpi-ffi/ghdl-vffi/test/EnvironmentVariables.vhd
@@ -18,6 +18,19 @@ begin
 
   process
 
+--    -- int setenv(
+--    --   const char *name,
+--    --   const char *value,
+--    --   int overwrite
+--    -- );
+--    function c_setenv(
+--      name      : string;
+--      value     : string;
+--      overwrite : integer
+--    ) return integer is
+--    begin report "VHPIDIRECT c_setenv" severity failure; end;
+--    attribute foreign of c_setenv : function is "VHPIDIRECT c_setenv";
+
     impure function c_getenv(
        name : string
     ) return line is
@@ -28,6 +41,10 @@ begin
     test_runner_setup(runner, runner_cfg);
 
     report c_getenv("PATH").all severity note;
+
+    --report to_string(c_setenv("MYKEY", "somevalue", 1)) severity note;
+
+    --report c_getenv("MYKEY") severity note;
 
     test_runner_cleanup(runner);
     wait;
