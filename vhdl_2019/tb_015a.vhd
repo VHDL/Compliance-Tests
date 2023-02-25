041828e350eabdf582903fa34911b8da88cbf615
Brian Padalino <bpadalino@gmail.com>
2023-02-25 11:40:08 -0500

    Add exaple for tb_015a.vhd

--- vhdl_2019/tb_015a.vhd
+++ vhdl_2019/tb_015a.vhd
@@ -1,3 +1,20 @@
 -- LCS-2016-015a: Standard functions to report current file name
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_015a
--- TODO
+entity e015a is
+end entity ;
+
+architecture arch of e015a is
+
+    use std.env.all ;
+
+begin
+
+    tb : process
+    begin
+        report "FILE_NAME: " & FILE_NAME ;
+        report "FILE_PATH: " & FILE_PATH ;
+        report "FILE_LINE: " & FILE_LINE ;
+        stop ;
+    end process ;
+
+end architecture ;
