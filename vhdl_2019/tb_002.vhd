a86bed8b6c3de8c6a07d9789581f9ee4638b1984
Brian Padalino <bpadalino@gmail.com>
2023-02-25 10:22:45 -0500

    Add example for tb_002.vhd

--- vhdl_2019/tb_002.vhd
+++ vhdl_2019/tb_002.vhd
@@ -1,3 +1,34 @@
 -- LCS-2016-002: Allow access and protected type parameters on function interfaces
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_002
--- TODO
+package pack002 is
+
+    type test_t is protected
+        procedure increment ;
+        impure function get return integer ;
+    end protected ;
+
+    impure function doit(variable x : inout test_t) return integer ;
+
+end package ;
+
+package body pack002 is
+
+    impure function doit(variable x : inout test_t) return integer is
+    begin
+        x.increment ;
+        return x.get ;
+    end function ;
+
+    type test_t is protected body
+        variable val : integer ;
+        procedure increment is
+        begin
+            val := val + 1 ;
+        end procedure ;
+        impure function get return integer is
+        begin
+            return val ;
+        end function ;
+    end protected body ;
+
+end package body ;
