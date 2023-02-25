e7093c9c152757c4d00530a438de447d07fbe2ac
Brian Padalino <bpadalino@gmail.com>
2023-02-25 12:03:23 -0500

    Adding example for tb_004.vhd

--- vhdl_2019/tb_004.vhd
+++ vhdl_2019/tb_004.vhd
@@ -1,3 +1,23 @@
 -- LCS-2016-004: Allow protected type method parameters to be access types, protected types (functions), and file types
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_004
--- TODO
+use std.textio.all ;
+
+package pack004 is
+
+    type test_t is protected
+        impure function something(variable x : inout text) return integer ;
+    end protected ;
+
+end package ;
+
+package body pack004 is
+
+    type test_t is protected body
+        impure function something(variable x : inout text) return integer is
+        begin
+            return 0;
+        end function ;
+    end protected body ;
+
+end package body ;
+
