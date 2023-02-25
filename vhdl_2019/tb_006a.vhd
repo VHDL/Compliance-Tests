5c457321f4595746a9d7d5047f3be77c3f6bfe3b
Brian Padalino <bpadalino@gmail.com>
2023-02-25 12:27:15 -0500

    Adding example for tb_006a.vhd
    Also added a file to be used, but unsure if this is the right
    place for it.

--- vhdl_2019/tb_006a.vhd
+++ vhdl_2019/tb_006a.vhd
@@ -1,3 +1,38 @@
 -- LCS-2016-006a: File IO/TextIO Updates
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_006a
--- TODO
+
+use std.textio.all ;
+
+entity e006a is
+end entity ;
+
+architecture arch of e006a is
+
+    use std.textio.all ;
+
+begin
+
+    tb : process
+        constant fname : string := "test006a.txt";
+        file f : text ;
+    begin
+        file_open(f, fname, READ_WRITE_MODE) ;
+        report "  state     : " & to_string(file_state(f)) ;
+        report "  mode      : " & to_string(file_mode(f)) ;
+        report "  size      : " & to_string(file_size(f)) ;
+        report "  canseek   : " & to_string(file_canseek(f)) ;
+        report "  position  : " & to_string(file_position(f)) ;
+        if file_canseek(f) = true then
+            file_seek(f, file_size(f)) ;
+            report "  position  : " & to_string(file_position(f)) ;
+            file_rewind(f) ;
+            report "  position  : " & to_string(file_position(f)) ;
+        end if ;
+        file_truncate(f, file_size(f)*2) ;
+        report "  size      : " & to_string(file_size(f)) ;
+        file_close(f) ;
+        report "  state     : " & to_string(file_state(f)) ;
+        std.env.stop ;
+    end process ;
+
+end architecture ;
