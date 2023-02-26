dee6271578b84af4ba43c70b4ce7049423aca0f9
Brian Padalino <bpadalino@gmail.com>
2023-02-26 11:53:55 -0500

    Adding example for tb_018a.vhd

--- vhdl_2019/tb_018a.vhd
+++ vhdl_2019/tb_018a.vhd
@@ -1,3 +1,11 @@
 -- LCS-2016-018a: New Attribute - 'INDEX
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_018a
--- TODO
+package pack018a is
+
+    type bigarray_t is array(natural range <>, positive range <>, character range <>) of integer ;
+
+    signal idx1 : bigarray_t'index(1) ;
+    signal idx2 : bigarray_t'index(2) ;
+    signal idx3 : bigarray_t'index(3) ;
+
+end package ;
