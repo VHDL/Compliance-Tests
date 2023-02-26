ca333446e0218058335a8a16ceaa1f95a165d59c
Brian Padalino <bpadalino@gmail.com>
2023-02-26 11:57:55 -0500

    Adding example for tb_018d.vhd

--- vhdl_2019/tb_018d.vhd
+++ vhdl_2019/tb_018d.vhd
@@ -1,3 +1,8 @@
 -- LCS-2016-018d: New attribute - 'DESGINATED_TYPE
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_018d
--- TODO
+package pack018d is
+
+    subtype ac_type is std.textio.line'designated_subtype ;
+    subtype ft_type is std.textio.text'designated_subtype ;
+
+end package ;
