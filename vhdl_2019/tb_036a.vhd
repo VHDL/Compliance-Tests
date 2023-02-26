e8c5281fa5f8f38bd367e2dbf6f90779975e46b8
Brian Padalino <bpadalino@gmail.com>
2023-02-26 12:39:10 -0500

    Add example for tb_036a.vhd

--- vhdl_2019/tb_036a.vhd
+++ vhdl_2019/tb_036a.vhd
@@ -1,3 +1,11 @@
 -- LCS-2016-036a: Allow for conditional expressions in a declaration (baseline)
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_036a
--- TODO
+
+package pack036a is
+  generic (
+    USE_LONG_DELAY : boolean ;
+  ) ;
+
+    constant delay : time := 1 ms when USE_LONG_DELAY = true else 1 ns ;
+
+end package ;
