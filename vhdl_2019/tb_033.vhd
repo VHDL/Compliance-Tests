a18fe0cf62b533c942a07d552eb76f45239d9fb0
Brian Padalino <bpadalino@gmail.com>
2023-02-26 12:35:26 -0500

    Adding example for tb_033.vhd

--- vhdl_2019/tb_033.vhd
+++ vhdl_2019/tb_033.vhd
@@ -1,3 +1,14 @@
 -- LCS-2016-033: Composition with Protected Types
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_033
--- TODO
+package pack033 is
+
+    type sometype_t is protected
+        procedure write(x : string) ;
+    end protected ;
+
+    type test_t is protected
+        private variable name : sometype_t ;
+        alias write is name.write[string] ;
+    end protected ;
+
+end package ;
