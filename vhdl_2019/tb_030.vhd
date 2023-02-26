6fe7566daa3911a22cb96cc4cc9c5cd84abd1472
Brian Padalino <bpadalino@gmail.com>
2023-02-26 12:21:30 -0500

    Adding example for tb_030.vhd
    Needs review.

--- vhdl_2019/tb_030.vhd
+++ vhdl_2019/tb_030.vhd
@@ -1,3 +1,25 @@
 -- LCS-2016-030: Garbage Collection
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_030
--- TODO
+-- REVIEW
+entity e030 is
+end entity ;
+
+architecture arch of e030 is
+
+    impure function bigstring return std.textio.line is
+    begin
+        return new string(1 to 1000000) ;
+    end function ;
+
+begin
+
+    tb : process
+        variable l : std.textio.line ;
+    begin
+        for i in 1 to 1000000 loop
+            l := bigstring ;
+        end loop ;
+        std.env.stop ;
+    end process ;
+
+end architecture ;
