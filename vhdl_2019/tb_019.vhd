9c403aff976b3f226fbe4d8013cfc9b1586c3a28
Brian Padalino <bpadalino@gmail.com>
2023-02-26 12:09:44 -0500

    Adding example for tb_019.vhd

--- vhdl_2019/tb_019.vhd
+++ vhdl_2019/tb_019.vhd
@@ -1,3 +1,25 @@
 -- LCS-2016-019: Inferring Constraints from Initial Values for Signals and Variables
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_019
--- TODO
+
+library ieee ;
+use ieee.fixed_pkg.all ;
+
+entity e019 is
+  port (
+    a   : in  sfixed ;
+    b   : in  sfixed ;
+    c   : out sfixed
+  ) ;
+end entity ;
+
+architecture arch of e019 is
+
+begin
+
+    process
+        variable prod : sfixed := a * b ;
+    begin
+        c <= resize(prod,a) ;
+    end process ;
+
+end architecture ;
