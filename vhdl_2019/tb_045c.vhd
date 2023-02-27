5438bcf625e1278c7d4e29af0c5efde2515fa204
Brian Padalino <bpadalino@gmail.com>
2023-02-26 23:05:49 -0500

    Adding example for tb_045c.vhd

--- vhdl_2019/tb_045c.vhd
+++ vhdl_2019/tb_045c.vhd
@@ -1,3 +1,21 @@
 -- LCS-2016-045c: Interface - 'CONVERSE for a mode view
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_045c
--- TODO
+package pack045c is
+
+    type rec_t is record
+        a   :   integer ;
+        b   :   string ;
+        c   :   bit_vector(7 downto 0) ;
+        d   :   bit_vector(0 to -1) ;
+    end record ;
+
+    view master of rec_t is
+        a   :   in ;
+        b   :   out ;
+        c   :   out ;
+        d   :   inout ;
+    end view ;
+
+    alias slave is master'converse ;
+
+end package ;
