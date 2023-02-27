eb376f9bcb549ff12068f89003276f35656202cc
Brian Padalino <bpadalino@gmail.com>
2023-02-26 23:01:37 -0500

    Adding example for tb_061.vhd

--- vhdl_2019/tb_061.vhd
+++ vhdl_2019/tb_061.vhd
@@ -1,3 +1,12 @@
 -- LCS-2016-061: Conditional Compilation
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_061
--- TODO
+use std.env.all ;
+package pack061 is
+
+    `if TOOL_TYPE = "SIMULATION" then
+        constant simulator : string := TOOL_NAME ;
+    `elsif TOOL_TYPE = "SYNTHESIS" then
+        constant synthesizer : string := TOOL_NAME ;
+    `end
+
+end package ;
