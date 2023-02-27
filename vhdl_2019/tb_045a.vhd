74ed467ef5fde5c9e76b4a756a3e220cfd88402a
Brian Padalino <bpadalino@gmail.com>
2023-02-26 23:06:54 -0500

    Adding example for tb_045a.vhd

--- vhdl_2019/tb_045a.vhd
+++ vhdl_2019/tb_045a.vhd
@@ -1,3 +1,19 @@
 -- LCS-2016-045a: Interface - defines new mode view construct for composite interface objects
 -- http://www.eda-twiki.org/cgi-bin/view.cgi/P1076/LCS2016_045a
--- TODO
+package pack045a is
+
+    type rec_t is record
+        a   :   integer ;
+        b   :   string ;
+        c   :   bit_vector(7 downto 0) ;
+        d   :   bit_vector(0 to -1) ;
+    end record ;
+
+    view master of rec_t is
+        a   :   in ;
+        b   :   out ;
+        c   :   out ;
+        d   :   inout ;
+    end view ;
+
+end package ;
