a2d8f429c4f8e347043b2202035d521dbc0b89ab
umarcor <unai.martinezcorral@ehu.eus>
2023-02-27 22:27:57 +0100

    run.py: update
    Co-Authored-By: Lars Asplund <lars.anders.asplund@gmail.com>

--- run.py
+++ run.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-import sys
 from pathlib import Path
-from vunit import VUnit, VUnitCLI
+from vunit import VUnit
 
 root = Path(__file__).resolve().parent
 
+# Hint:
+#   VUnit supports adding custom CLI arguments.
+#   See http://vunit.github.io/py/ui.html#adding-custom-command-line-arguments.
+
 ui = VUnit.from_argv(compile_builtins=False, vhdl_standard="2008")
 
-if ui.get_simulator_name() in ["rivierapro", "activehdl"]:
+if ui.get_simulator_name() in [
+    "activehdl",
+    "nvc",
+    "rivierapro",
+]:
     ui = VUnit.from_argv(compile_builtins=False, vhdl_standard="2019")
-    print("Using VHDL-2019")
+
+print(f"Using VHDL-{ui.vhdl_standard}")
 
 ui.add_vhdl_builtins()
 
@@ -27,4 +35,4 @@ ui.set_compile_option("activehdl.vcom_flags", ["-dbg"])
 try:
     ui.main()
 except SystemExit:
-    sys.exit(0)
+    pass
