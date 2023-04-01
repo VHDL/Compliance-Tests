2f7367ced601f6860a1c90a0f8e402346d4d025a
oliverbm67 <obetschi@hawk.iit.edu>
2023-04-01 16:39:29 +0200

    add hex code test and update sensitivity

--- vhdl_2008/tb_sensitivity_all.vhd
+++ vhdl_2008/tb_sensitivity_all.vhd
@@ -25,9 +25,10 @@ architecture arch of tb_sensitivity_all is
     signal in_a         : bit;
     signal in_b         : bit;
     signal in_c         : bit;
+    signal out_a        : bit;
 
 begin
-    dut_inst : entity work.dut_sensitivity_all port map (in_a => in_a, in_b => in_b, in_c => in_c);
+    dut_inst : entity work.dut_sensitivity_all port map (in_a => in_a, in_b => in_b, in_c => in_c, out_a => out_a);
 
     test_runner : process begin
         test_runner_setup(runner, runner_cfg);
