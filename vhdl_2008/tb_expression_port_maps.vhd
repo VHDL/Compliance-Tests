8014a30088b8ac6e5e82e1c162558909ac1955eb
oliverbm67 <obetschi@hawk.iit.edu>
2023-04-23 11:54:02 +0200

    Add a check for tests missing one

--- vhdl_2008/tb_expression_port_maps.vhd
+++ vhdl_2008/tb_expression_port_maps.vhd
@@ -1,7 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity expression_port_maps is
     Port (
-        ina        : in bit;
-        outa       : out bit
+        ina        : in std_logic;
+        outa       : out std_logic
     );
 end expression_port_maps;
 architecture dut_arch of expression_port_maps is
@@ -11,22 +14,29 @@ end dut_arch;
 
 library vunit_lib ;
 context vunit_lib.vunit_context;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity tb_expression_port_maps is
   generic ( runner_cfg : string ) ;
 end entity;
 architecture arch of tb_expression_port_maps is
 
-    signal ina          : bit;
-    signal inb         : bit;
-    signal outa         : bit;
+    signal ina          : std_logic;
+    signal inb         : std_logic;
+    signal outa         : std_logic;
 
 begin
     dut_inst : entity work.expression_port_maps port map (ina => (ina or inb), outa => outa);
 
     test_runner : process begin
+        ina <= '1';
+        inb <= '0';
         test_runner_setup(runner, runner_cfg);
         test_runner_cleanup(runner);
+        check_equal(outa,'1');
+        ina <= '0';
+        check_equal(outa,'0');
         std.env.stop ;
     end process;
 end architecture;
--- vhdl_2008/tb_read_out_port.vhd
+++ vhdl_2008/tb_read_out_port.vhd
@@ -1,34 +1,42 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity read_out_port is
     Port (
-        ina        : in bit;
-        outa       : out bit;
-        outb       : out bit
+        ina        : in std_logic;
+        outa       : out std_logic;
+        outb       : out std_logic
     );
 end read_out_port;
 architecture dut_arch of read_out_port is
 begin
     outa <= ina;
-    outb <= outa;
+    outb <= not outa;
 end dut_arch;
 
 library vunit_lib ;
 context vunit_lib.vunit_context;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity tb_read_out_port is
   generic ( runner_cfg : string ) ;
 end entity;
 architecture arch of tb_read_out_port is
 
-    signal ina          : bit;
-    signal outa         : bit;
-    signal outb         : bit;
+    signal ina          : std_logic;
+    signal outa         : std_logic;
+    signal outb         : std_logic;
 
 begin
     dut_inst : entity work.read_out_port port map (ina => ina, outa => outa, outb => outb);
 
     test_runner : process begin
+        ina <= '1';
         test_runner_setup(runner, runner_cfg);
         test_runner_cleanup(runner);
+        check_equal(outa, '1');
+        check_equal(outb, '0');
         std.env.stop ;
     end process;
 end architecture;
